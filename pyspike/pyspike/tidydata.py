52a37b0554bef4965b73e8a75f9eb190e8e9fc68
Rob Walton <rob.walton@eng.ox.ac.uk>
2018-11-29 11:53:54 +0000

    Create tidydata.generate_state_change_frame
    Works only for places so far

--- pyspike/pyspike/tidydata.py
+++ pyspike/pyspike/tidydata.py
@@ -56,3 +56,25 @@ def filter_by_name(frame, name_list):
     return frame
 
 
+def insert_step_column_based_on_unique_time_vals(places):
+    z = {v: i for i, v in enumerate(places['time'].unique())}
+    places.insert(0, 'step', places['time'].map(z))
+
+
+def generate_state_change_frame(df):
+    df.sort_values(by=['time'])
+    change_frame_list = []
+
+    for num in df.num.unique():
+        for state_name in df.name.unique():
+            df_num_name = df[(df.num == num) & (df.name == state_name)]
+            df_num_name_changed = df_num_name[df_num_name['count'].diff() != 0]
+            df_num_name_entered = df_num_name_changed[df_num_name_changed['count'] == 1]
+            change_frame_list.append(df_num_name_entered)
+
+
+    # concatenate
+    df_out = pd.concat(change_frame_list)
+    del df_out['count']
+    return df_out.sort_values(by=['num', 'time'])
+
