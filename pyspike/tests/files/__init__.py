32f6424547da860ff7edc276ccc1743c156ce0a6
Rob Walton <rob.walton@eng.ox.ac.uk>
2018-12-18 12:01:02 +0000

    Add joint 1 and 2 neighbour simulation and failing test showing incorrect temporal graph inference

--- pyspike/tests/files/__init__.py
+++ pyspike/tests/files/__init__.py
@@ -20,3 +20,9 @@ RUN_90 = Path(__file__).parent / 'run_90_are_both_neighbours'
 RUN_90_PLACES = RUN_90 / 'places.csv'
 RUN_90_TRANSITIONS = RUN_90 / 'transitions.csv'
 RUN_90_GML = RUN_90 / '3-triangle-truss.gml'
+
+# Last registered both single and two neighbour following on tos network
+RUN_100 = Path(__file__).parent / 'run_100'
+RUN_100_PLACES = RUN_100 / 'places.csv'
+RUN_100_TRANSITIONS = RUN_100 / 'transitions.csv'
+RUN_100_GML = RUN_100 / 'tos-network.gml'
