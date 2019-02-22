ac63cf601fba0928ed61b410164e42d28ca65128
Rob Walton <rob.walton@eng.ox.ac.uk>
2019-02-22 10:23:15 +0000

    Add basic setup.py and complete requirements.txt

---
+++ pyspike/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='proof-of-concept',
+    version='',
+    packages=['tests', 'tests.files', 'pyspike', 'pyspike.sacred', 'websci19', 'websci19.tests'],
+    package_dir={'': 'src/pyspike'},
+    url='',
+    license='',
+    author='walton',
+    author_email='',
+    description=''
+)
