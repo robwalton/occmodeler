95576b23350ce7b62b5f609e9a026eda2334d7d9
Rob Walton <rob.walton@eng.ox.ac.uk>
2019-03-05 17:12:35 +0000

    Refactor SacredRun to make (json) serialisable

--- pyspike/pyspike/sacred/sacredrun.py
+++ pyspike/pyspike/sacred/sacredrun.py
@@ -7,30 +7,64 @@ class SacredRun:
     def __init__(self, root_run_path: Path):
         assert root_run_path.exists()
         assert root_run_path.is_dir()
-        self.run_path = root_run_path
-        self.config_path = root_run_path / 'config.json'
-        self.cout_path = root_run_path / 'cout.txt'
-        self.metric_path = root_run_path / 'metric.json'
-        self.places_path = root_run_path / 'places.csv'
-        self.run_path = root_run_path / 'run.csv'
-        self.transitions_path = root_run_path / 'transitions.csv'
-        self.medium_graph = root_run_path / 'medium_graph.gml'
+        self.root_run_path_string = str(root_run_path)
+
+    def __eq__(self, other):
+        return self.root_run_path_string == other.root_run_path_string
+
+    # Paths
+
+    @property
+    def root_path(self):
+        return Path(self.root_run_path_string)
+
+    @property
+    def config_path(self):
+        return self.root_path / 'config.json'
+
+    @property
+    def places_path(self):
+        return self.root_path / 'places.csv'
+
+    @property
+    def run_path(self):
+        return self.root_path / 'run.csv'
+
+    @property
+    def transitions_path(self):
+        return self.root_path / 'transitions.csv'
+
+    @property
+    def medium_graph_path(self):
+        return self.root_path / 'medium_graph.gml'
+
+    # requiring file access the following could be cached too
 
     @property
     def config(self):
         with open(self.config_path) as f:
             return json.load(f)
 
+    # TODO: consider putting medium_graph on here
 
 
-    # places = tidydata.read_csv(filename=str(places_path), node_type="place", drop_non_coloured_sums=True)
-    # transitions = tidydata.read_csv(filename=str(transitions_path), node_type="transition", drop_non_coloured_sums=True)
-    # _, _, tstep = tidydata.determine_time_range_of_data_frame(places)
-    # places = pyspike.tidydata.prepend_tidy_frame_with_tstep(places)
-    # transitions = pyspike.tidydata.prepend_tidy_frame_with_tstep(transitions)
-    #
-    # place_change_events = pyspike.temporal.generate_place_change_events(places)
-    # transition_events = pyspike.temporal.generate_transition_events(transitions)
-    #
-    # causal_graph = pyspike.temporal.generate_causal_graph(
-    #     place_change_events, transition_events, time_per_step=tstep)
+def _encode_sacredrun(obj):
+    if isinstance(obj, SacredRun):
+        return dict(__sacredrun__=True, root_path_string=obj.root_run_path_string)
+    else:
+        type_name = obj.__class__.__name__
+        raise TypeError(f"Object of type '{type_name}' is not JSON serializable")
+
+
+def _decode_sacredrun(dct):
+    if "__sacredrun__" in dct:
+        return SacredRun(Path(dct["root_path_string"]))
+    return dct
+
+
+def sacredrun_to_json(run):
+    return json.dumps(run, default=_encode_sacredrun)
+
+
+def json_to_sacredrun(data):
+    return json.loads(data, object_hook=_decode_sacredrun)
