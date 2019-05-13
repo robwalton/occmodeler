f64d1e128ba23a8172f2012273ed5da110c4199e
Rob Walton <rob.walton@eng.ox.ac.uk>
2019-05-13 12:34:59 +0100

    Complete move of SystemModel
    (Automated testing would help!)

--- occ/occ/model/__init__.py
+++ occ/occ/model/__init__.py
@@ -1,4 +1,8 @@
-from pyspike.model import UnitModel, u, n1, n2, Unit, Place, Transition
+from dataclasses import dataclass
+
+import networkx as nx
+
+from pyspike.model import UnitModel, u, n1, n2, Unit, Place, Transition, Color, marking
 from pyspike.exe import SimArgs
 
 from occ.model.transitions import FollowNeighbour as follow1
@@ -6,4 +10,13 @@ from occ.model.transitions import FollowTwoNeighbours as follow2
 from occ.model.transitions import External as ext
 
 
-__all__ = ['u', 'n1', 'n2', 'Unit', 'UnitModel', 'follow1', 'follow2', 'ext', 'Place', 'Transition', 'SimArgs']
+__all__ = ['u', 'n1', 'n2', 'Unit', 'UnitModel', 'follow1', 'follow2', 'ext', 'Place', 'Transition', 'SimArgs',
+           'SystemModel', 'Color', 'marking']
+
+
+@dataclass(frozen=True)
+class SystemModel:
+    unit: UnitModel
+    network: nx.Graph  # multiple later. depends on unit
+    network_name: str
+    marking: dict = None
