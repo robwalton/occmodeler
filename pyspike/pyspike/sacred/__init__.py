d2cffbe9d023eaa9984e64eda09c670547455425
Rob Walton <rob.walton@eng.ox.ac.uk>
2019-07-09 10:58:34 +0100

    Remove leftover sacred components

--- pyspike/pyspike/sacred/__init__.py
+++
@@ -1,8 +0,0 @@
-
-
-TMP_SPIKE_INPUT_DIR = '_spike/input'
-TMP_SPIKE_CONF_PATH = '_spike/input/conf.spc'
-TMP_SPIKE_INPUT_CANDL_PATH = '_spike/input/generated.candl'
-
-
-TMP_SPIKE_OUTPUT_DIR = '_spike/output'
